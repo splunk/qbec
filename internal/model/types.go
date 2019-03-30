a27bb7d9e84ca29c9d3c306bf1365ec58b939d56
gotwarlost <krishnan@the-krishnans.com>
2019-03-29 19:32:11 -0700

    define variable interface

--- internal/model/types.go
+++ internal/model/types.go
@@ -26,6 +26,38 @@ type Environment struct {
 	Excludes         []string `json:"excludes,omitempty"` // additional components to exclude for this env
 }
 
+// Variable is a late-bound value that is not known in advance (e.g. an image tag in a CI build) or should not be
+// checked into source control (e.g. secrets). The name is an identifier.
+//
+// The value of the variable, if not passed in explicitly, is derived from an environment variable of the form
+// QBEC_VAR_MODIFIED_NAME where MODIFIED_NAME is uppercase version of the variable name converted to snake-case.
+// (e.g. a variable with name fooBar is picked up from the QBEC_VAR_FOO_BAR environment variable).
+//
+// In the jsonnet code, the value of the variable can be gotten from the external variable called `vars.qbec.io/name`
+// (e.g. the fooBar variable can be accessed as std.extVar('vars.qbec.io/fooBar')).
+//
+// Variables must NOT have a blank value. A blank string is treated as equivalent to not setting it.
+//
+// `qbec apply` MUST set all variables at the time of invoking the command either via environment variables
+// or on the command line. Other commands (including `qbec apply --dry-run`) need to only set the variables
+// that do not have a default value.
+//
+// A variable may be tagged as a secret in which case it can only be set from the
+// environment and its value is obfuscated by default (this may be displayed using the --show-secrets flag).
+//
+// Variables may be optionally associated to components. Doing this has the following implications.
+//
+// - The variable value is available only for the associated components.
+//
+// - The caller need not set the variable value if all of its associated components have been excluded from the
+//   apply command using component filters.
+type Variable struct {
+	Name       string   `json:"name"`                 // variable name
+	Default    string   `json:"default,omitempty"`    // default value, blank requires the variable to be set.
+	Components []string `json:"components,omitempty"` // components that use the variable, optional
+	Secret     bool     `json:"secret"`               // true if the variable's value is a secret
+}
+
 // AppMeta is the simplified metadata object for a qbec app.
 type AppMeta struct {
 	// required: true
@@ -46,6 +78,8 @@ type AppSpec struct {
 	Excludes []string `json:"excludes,omitempty"`
 	// list of library paths to add to the jsonnet VM at evaluation
 	LibPaths []string `json:"libPaths,omitempty"`
+	// list of late-bound variables for the app
+	Vars []Variable `json:"vars,omitempty"`
 }
 
 // QbecApp is a set of components that can be applied to multiple environments with tweaked runtime configurations.
