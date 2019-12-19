a039dd3fedb5254c530baee7ce6e691f356a4e12
gotwarlost <krishnan@the-krishnans.com>
2019-12-19 11:20:13 -0800

    fix merge patch to correctly handle non-existent props for environment

--- internal/commands/env_test.go
+++ internal/commands/env_test.go
@@ -32,9 +32,10 @@ func TestEnvListBasic(t *testing.T) {
 	require.Nil(t, err)
 	lines := strings.Split(strings.Trim(s.stdout(), "\n"), "\n")
 	a := assert.New(t)
-	require.Equal(t, 2, len(lines))
+	require.Equal(t, 3, len(lines))
 	a.Equal("dev", lines[0])
 	a.Equal("prod", lines[1])
+	a.Equal("stage", lines[2])
 }
 
 func TestEnvListYAML(t *testing.T) {
