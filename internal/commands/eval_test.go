3b7d0db1f7a72597e30057cdb383afb08506d4d0
gotwarlost <krishnan@the-krishnans.com>
2021-02-01 18:14:00 -0800

    add eval tests

---
+++ internal/commands/eval_test.go
@@ -0,0 +1,101 @@
+/*
+   Copyright 2021 Splunk Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package commands
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEvalBasic(t *testing.T) {
+	s := newScaffold(t)
+	defer s.reset()
+	err := s.executeCommand("eval", "misc/simple.jsonnet")
+	require.NoError(t, err)
+	var data map[string]interface{}
+	err = s.jsonOutput(&data)
+	require.NoError(t, err)
+
+	a := assert.New(t)
+	a.Equal("str", data["foo"])
+	a.Equal(true, data["bar"])
+}
+
+func TestEvalVars(t *testing.T) {
+	s := newScaffold(t)
+	defer s.reset()
+	err := s.executeCommand("eval", "misc/vars.jsonnet", "--vm:ext-str", "foo=str", "--vm:ext-code", "bar=true")
+	require.NoError(t, err)
+	var data map[string]interface{}
+	err = s.jsonOutput(&data)
+	require.NoError(t, err)
+
+	a := assert.New(t)
+	a.Equal("str", data["foo"])
+	a.Equal(true, data["bar"])
+}
+
+func TestEvalTLAs(t *testing.T) {
+	s := newScaffold(t)
+	defer s.reset()
+	err := s.executeCommand("eval", "misc/tla.jsonnet", "--vm:tla-str", "foo=str", "--vm:tla-code", "bar=true")
+	require.NoError(t, err)
+	var data map[string]interface{}
+	err = s.jsonOutput(&data)
+	require.NoError(t, err)
+
+	a := assert.New(t)
+	a.Equal("str", data["foo"])
+	a.Equal(true, data["bar"])
+}
+
+func TestEvalBasicYAML(t *testing.T) {
+	s := newScaffold(t)
+	defer s.reset()
+	err := s.executeCommand("eval", "misc/simple.jsonnet", "--format=yaml")
+	require.NoError(t, err)
+	d, err := s.yamlOutput()
+	require.NoError(t, err)
+	require.Equal(t, 1, len(d))
+	data, ok := d[0].(map[string]interface{})
+	require.True(t, ok)
+	a := assert.New(t)
+	a.Equal("str", data["foo"])
+	a.Equal(true, data["bar"])
+}
+
+func TestEvalInQBECContext(t *testing.T) {
+	s := newScaffold(t)
+	defer s.reset()
+	err := s.executeCommand("eval", "misc/qbec.jsonnet", "--env=dev")
+	require.NoError(t, err)
+	var data map[string]interface{}
+	err = s.jsonOutput(&data)
+	a := assert.New(t)
+	a.Equal("dev", data["foo"])
+	a.Equal("development", data["bar"])
+}
+
+func TestEvalBarArgs(t *testing.T) {
+	s := newScaffold(t)
+	defer s.reset()
+	err := s.executeCommand("eval", "misc/qbec.jsonnet", "misc/simple.jsonnet")
+	require.Error(t, err)
+	assert.Equal(t, "exactly one file required", err.Error())
+}
