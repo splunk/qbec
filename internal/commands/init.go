31ae5f325503f96f077bd01270734164634d727f
Andrei Kvapil <kvapss@gmail.com>
2021-09-08 12:41:07 +0200

    automatic params.libsonnet
    Co-authored-by: Krishnan Anantheswaran <krishnan@the-krishnans.com>

--- internal/commands/init.go
+++ internal/commands/init.go
@@ -65,16 +65,14 @@ base {
 
 var paramsTemplate = template.Must(template.New("any-env").Parse(`
 // this file returns the params for the current qbec environment
-// you need to add an entry here every time you add a new environment.
-
 local env = std.extVar('qbec.io/env');
-local paramsMap = {
-  _: import './environments/base.libsonnet',
-  default: import './environments/default.libsonnet',
-};
+local paramsMap = import 'glob-import:environments/*.libsonnet';
+local baseFile = if env == '_' then 'base' else env;
+local key = 'environments/%s.libsonnet' % baseFile;
 
-if std.objectHas(paramsMap, env) then paramsMap[env] else error 'environment ' + env + ' not defined in ' + std.thisFile
-
+if std.objectHas(paramsMap, key)
+then paramsMap[key]
+else error 'no param file %s found for environment %s' % [key, env]
 `))
 
 var componentExampleTemplate = template.Must(template.New("comp").Parse(`
